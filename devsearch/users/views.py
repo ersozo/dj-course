3538c20e989834f8e44c036b7c8f3710f429ba76
ozgur <ozgur.ersoz3@gmail.com>
2024-09-19 17:49:58 +0300

    auth

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .models import Profile
+from django.contrib.auth.models import User
+
 
 from .models import Profile
 
 
 # Create your views here.
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("profiles")
+        else:
+            print("Username or password is incorrect")
+
+    return render(request, template_name="users/login_register.html")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {"profiles": profiles}

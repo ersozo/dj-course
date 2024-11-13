de8c6fd585c37aa50ff5b99b1d8b30ca732c7ddf
ozgur <ozgur.ersoz3@gmail.com>
2024-11-13 17:56:25 +0300

    serializers

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_routes),
+    path("projects/", views.get_projects),
+    path("projects/<str:pk>/", views.get_project),
 ]
--- devsearch/api/views.py
+++ devsearch/api/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
 
 
+@api_view(["GET"])
 def get_routes(request):
     routes = [
         {"GET": "/api/projects/"},
@@ -10,4 +15,18 @@ def get_routes(request):
         {"POST": "/api/users/token/refresh"},
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def get_projects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_project(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
